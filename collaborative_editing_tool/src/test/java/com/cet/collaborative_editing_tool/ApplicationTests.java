e673920797dc3e1f63ae96641792d8fd191fa996
P-Odiix <mahingarodin@gmail.com>
2025-05-27 16:38:45 +0200

    [changes]: initial setup of the backend

---
+++ collaborative_editing_tool/src/test/java/com/cet/collaborative_editing_tool/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cet.collaborative_editing_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
